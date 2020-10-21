930545040ebc6b0f132c1d51f930ed79e1db3be4
Maisa Maximo <b.outmaself@gmail.com>
2020-10-21 00:09:52 -0300

    create table images

---
+++ backend/src/database/migrations/1603249394745-create_images.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createImages1603249394745 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "images",
+        columns: [
+          {
+            name: "id",
+            type: "integer",
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "path",
+            type: "varchar",
+          },
+          {
+            name: "orphanage_id",
+            type: "integer",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
